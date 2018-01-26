f1ede31ce339a5741eec2d266c091e81de225199
= <=>
2018-01-25 23:48:01 -0500

    comment hanged

--- app/src/main/java/model/Computer.java
+++ app/src/main/java/model/Computer.java
@@ -32,7 +32,7 @@ public class Computer {
         movesLeft = 4;
         pieces = new int[]{1,1,1,1};
     }
-    //and back we go
+    //i think i got it to work
     private int getMoves(Board b, Piece p) {
         int count = 0;
         int xPos = p.getXPos();
